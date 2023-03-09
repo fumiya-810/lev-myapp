c1e9c3cf1a415ac2dcf809c930bbf3d9dd964d28
fumiya-810 <fumiyasamo@yahoo.co.jp>
2023-03-09 13:37:29 +0900

    詳細画面実装開始

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,4 +11,10 @@ class PostController extends Controller
     public function index(Post $spot){
         return view('posts/index')->with(['spots' => $spot->get()]); 
     }
+    
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+     //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
 }
